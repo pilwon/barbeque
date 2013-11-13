4a7d60f0eb483480f818f2b6fac2ae0e9af29fe2
Pilwon Huh <pilwon@gmail.com>
2013-11-13 00:41:57 -0500

    Simplified Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,34 +19,18 @@ module.exports = function (grunt) {
         '!node_modules/**/*'
       ]
     },
-    mochaTest: {  // grunt-mocha-test
-      files: ['test/**/*.js']
-    },
-    mochaTestConfig: {  // grunt-mocha-test
-      options: {
-        reporter: 'nyan'
-      }
-    },
-    watch: {  // grunt-regarde (task renamed from regarde to watch)
+    watch: {  // grunt-contrib-watch
       all: {
         files: [
           '**/*.js',
           '!node_modules/**/*'
-        ],
-        tasks: 'test'
+        ]
       }
     }
   });
 
-  grunt.registerTask('test', [
-    'jshint',
-    'mochaTest',
-    'watch'
-  ]);
-
-  grunt.registerTask('t', 'test');
-
   grunt.registerTask('default', [
-    'test'
+    'jshint',
+    'watch'
   ]);
 };
